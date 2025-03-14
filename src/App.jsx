415cdbe0b51cfb393e7dfd724c8d55b463088b74
Shakhawat Hosen <125280128+Md-Shakhawat-Hosen@users.noreply.github.com>
2025-03-14 18:33:49 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return (
     <>
      
-      <h1 className='text-2xl font-bold'>Vite + React12345</h1>
+      <h1 className='text-2xl font-bold'>Vite + React1234</h1>
     </>
   )
 }
